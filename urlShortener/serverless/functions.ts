ee305ea1a34e742928b39eccc0404ad43aa92d5a
Andrei_Kniazev <andrei.v.kniasev@gmail.com>
2022-10-20 16:29:19 +0300

    add url shortener project

---
+++ urlShortener/serverless/functions.ts
@@ -0,0 +1,26 @@
+import type { AWS } from '@serverless/typescript'
+
+export const functions: AWS['functions'] = {
+  setUrl: {
+    handler: 'src/functions/setUrl/index.handler',
+    events: [
+      {
+        httpApi: {
+          path: '/',
+          method: 'post',
+        },
+      },
+    ],
+  },
+  getUrl: {
+    handler: 'src/functions/getUrl/index.handler',
+    events: [
+      {
+        httpApi: {
+          path: '/{code}',
+          method: 'get',
+        },
+      },
+    ],
+  },
+}
